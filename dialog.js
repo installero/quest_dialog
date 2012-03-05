4f0ee99fa55add78f54bb06e3678a18c2ab0007c
Vadim Venediktov <install.vv@gmail.com>
2012-03-05 19:28:48 +0400

    Ref: [JS] User specific code moved to lines.js

--- dialog.js
+++ dialog.js
@@ -1,63 +1,8 @@
-var DIALOG = {
-  words:'Привет, меня зовут Вадим Венедиктов!',
-  variants:[{
-    answer:'Я тут недавно пользовался вашим Таксовиком.',
-    words:'Ну и как?',
-    variants:[{
-      answer:'Очень клёво! Всем друзьям рекомендую!',
-      words:'Рад, что сделал полезный сервис. С тебя — пиво!',
-      variants:[{
-        answer:'Да без проблем!',
-        words:'Хе-хе!'
-      }]
-    },{
-      answer:'Нормально доехали! Но тарифы немного неточные.',
-      words:'Ну да, таксопарки часто меняют тарифы. Нам постоянно об этом сообщают, мы обновляем информацию.',
-      variants:[{
-        answer:'Прикольно. А как с монетизацией?',
-        words:'Да пока чё-то не очень.'
-      }]
-    }]
-  },{
-    answer:'Работу-то нашёл?',
-    words:'Нашёл. Потом уволили, правда.',
-    variants:[{
-      answer:'Где работал?',
-      words:'В «Аймобилко», технологом.',
-      variants:[{
-        answer:'Ого, это ты коллегой Чикуёнка был?',
-        words:'Не совсем. Я работал над другим проектом.',
-        variants:[{
-          answer:'Над каким?',
-          words:'Боюсь, это секрет.'
-        }]
-      },{
-        answer:'Первый раз слышу',
-        words:'Интернет-магазин по продаже мобильного контента. Ну, электронные книжки, фильмы, музыка, аудиокниги... Есть своя читалка для айфона и андройда.',
-        variants:[{
-          answer:'Ну и как тебе там?',
-          words:'Весёлые ребята. Интересно было поработать.'
-        }]
-      }]
-    }]
-  },{
-    answer:'Не женился ещё?',
-    words:'Нет, пока. Мир-то не захвачен ещё.',
-    variants:[{
-      answer:'А девушка есть?',
-      words:'Угу',
-      variants:[{
-        answer:'Познакомишь?',
-        words:'Всему своё время.'
-      }]
-    }]
-  }]
-};
 
 var current_dialog = DIALOG;
 
 $(function(){
-  $('.person').html('&mdash; '+current_dialog['words']);
+  $('.person').html(md(current_dialog['line']));
   drawVariants(current_dialog['variants']);
   bindVariants();
 });
@@ -68,7 +13,7 @@ function drawVariants(variants){
     var variant = variants[i];
     if(!variant['used']){
       var li = $('<li/>',{'class':'variant','data-variant-id':i}).appendTo('.variants');
-      $('<a/>',{href:'#'}).html('&mdash; '+variant['answer']).appendTo(li);
+      $('<a/>',{href:'#'}).html(md(variant['reply'])).appendTo(li);
     }
   };
 }
@@ -78,8 +23,8 @@ function bindVariants(){
     current_dialog = current_dialog['variants'][$(this).attr('data-variant-id')];
     current_dialog['used'] = true;
 
-    updateDialog('&mdash; '+current_dialog['answer']);
-    $('.person').html('&mdash; '+current_dialog['words']);
+    updateDialog(md(current_dialog['reply']));
+    $('.person').html(md(current_dialog['line']));
 
     if(!current_dialog['variants']) current_dialog = DIALOG;
 
@@ -93,3 +38,7 @@ function updateDialog(text){
   $('<li/>').html($('.person').html()).appendTo('.dialog');
   $('<li/>').html(text).appendTo('.dialog');
 };
+
+function md(text) {
+  return '&mdash; '+text;
+}
